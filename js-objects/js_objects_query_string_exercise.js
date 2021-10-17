707c0970bc5a0175a4f62854f500fa7775a44a41
paalso <40a.pvl@gmail.com>
2021-10-17 18:35:47 +0300

    JS Objects. Add some solutions

---
+++ js-objects/js_objects_query_string_exercise.js
@@ -0,0 +1,35 @@
+// https://ru.hexlet.io/challenges/js_objects_query_string_exercise
+// 
+
+// Javascript: Сборщик строки запроса
+// ===================================
+
+/*
+Query String (строка запроса) - часть адреса страницы в интернете, содержащая
+константы и их значения. Она начинается после вопросительного знака и идет до
+конца адреса.
+
+Пример:
+# query string: page=5
+https://ru.hexlet.io/blog?page=5
+Если параметров несколько, то они отделяются амперсандом &:
+
+# query string: page=5&per=10
+https://ru.hexlet.io/blog?per=10&page=5
+
+Реализуйте и экспортируйте функцию по умолчанию, которая принимает на вход список
+параметров и возвращает сформированный query string из этих параметров:
+*/
+
+const bqs = params => {
+    const tokens = Object.entries(params).sort().map(
+        ([key, val]) => `${key}=${val}`
+    );
+    return tokens.join('&');
+};
+
+
+console.log(bqs({ per: 10, page: 1 }));
+// page=1&per=10
+console.log(bqs({ param: 'all', param1: true }));
+// param=all&param1=true
