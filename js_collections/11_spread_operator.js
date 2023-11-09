96a7dae4d3e5ef8dd1a4e77734976603417b26ea
Pavel Soroka <40a.pvl@gmail.com>
2023-11-09 18:17:05 +0200

    Add js_collections destructuring exersice solution

---
+++ js_collections/11_spread_operator.js
@@ -0,0 +1,28 @@
+// https://ru.hexlet.io/courses/js_collections/lessons/destructuring/exercise_unit
+// https://ru.hexlet.io/code_reviews/272543
+
+// JS: Коллекции 
+// Дестракчеринг
+// ----------------------------------------------------------------------------------
+
+/*
+  Реализуйте и экспортируйте по умолчанию функцию, которая принимает на вход
+  список машин (в виде обычного js массива с объектами), а возвращает объект,
+  в котором свойство - это год выпуска, а значение - это количество машин для
+  соответствующего года.
+*/
+
+const getCarsCountByYear = data => data
+  .map(({year}) => year)
+  .reduce((acc, e) => ({...acc, [e]: (acc[e] ?? 0) + 1}), {});
+
+
+const cars = [
+  { brand: 'bmw', model: 'm5', year: 2014 },
+  { brand: 'bmw', model: 'm4', year: 2013 },
+  { brand: 'kia', model: 'sorento', year: 2014 },
+  { brand: 'kia', model: 'rio', year: 2010 },
+  { brand: 'kia', model: 'sportage', year: 2012 },
+];
+
+console.log(getCarsCountByYear(cars));
